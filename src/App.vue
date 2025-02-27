1247e385c21a490c263ed3cabc8dfc4cc2bd3468
jaeyo03 <toyazx@naver.com>
2025-02-27 12:35:29 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style lang="scss" scoped>
+.btn {
+  background-color: $primary-color-100;
+  color: white;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {RouterLink} from "vue-router";
+</script>
+
+<template>
+  <div>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <button class="btn">주요 색상 SCSS 사용하기</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  display: grid;
+}
+
+.btn {
+  background-color: $primary-color-100;
+  color: white;
+  padding: 10px 15px;
+  width: 300px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
