43e0cb5f6c1e9843fdb68ac661b5dd3db4bc10d6
jaeyo03 <toyazx@naver.com>
2025-02-27 17:47:47 +0900

    feat : 라우터 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
-import RegisterView from '../views/RegisterView.vue'
-import CommunityView from '../views/CommunityView.vue'
+import HomeView from '@/views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
+import CommunityView from '@/views/CommunityView.vue'
+import BookView from '@/views/BookView.vue'
+import LibraryView from '@/views/LibraryView.vue'
+import PerformanceView from '@/views/PerformanceView.vue'
+import ChallengeView from '@/views/ChallengeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +17,31 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: '/book',
+      name: 'book',
+      component: BookView,
+    },
+    {
+      path: '/library',
+      name: 'library',
+      component: LibraryView,
+    },
+    {
+      path: '/performance',
+      name: 'performance',
+      component: PerformanceView,
+    },
+    {
+      path: '/challenge',
+      name: 'challenge',
+      component: ChallengeView,
+    },
+    {
+      path: '/community',
+      name: 'community',
+      component: CommunityView,
+    },
+    {
       path: '/login',
       name: 'login',
       component: LoginView,
@@ -21,11 +50,6 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterView,
-    },
-    {
-      path: '/community',
-      name: 'community',
-      component: CommunityView,
     },
   ],
 })
