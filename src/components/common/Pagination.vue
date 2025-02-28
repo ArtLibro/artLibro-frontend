96ec02ed76a9ab307a90d608d1c1cdf622005510
Jiwoo <erase0250@gmail.com>
2025-02-28 14:14:24 +0900

    feat : 페이지네이션 공통 컴포넌트에 추가

---
+++ src/components/common/Pagination.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const current = ref(1)
+</script>
+
+<template>
+  <a-pagination v-model:current="current" :total="50" show-less-items />
+</template>
+
+<style lang="scss" scoped>
+/* 활성화된 페이지네이션 테두리 */
+:deep(.ant-pagination-item-active) {
+  border-color: $secondary-color-300 !important;
+}
+
+/* 활성화된 페이지네이션 폰트 색상 */
+:deep(.ant-pagination-item-active a) {
+  color: $secondary-color-300 !important;
+  font-weight: bold;
+}
+</style>
