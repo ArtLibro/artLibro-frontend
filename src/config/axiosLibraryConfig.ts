5d9b33fa4e71a6057350949dc430fb4d11e9f350
jaeyo03 <toyazx@naver.com>
2025-03-02 01:20:38 +0900

    feat : axios 타임아웃 수정

--- src/config/axiosLibraryConfig.ts
+++ src/config/axiosLibraryConfig.ts
@@ -4,7 +4,7 @@ const LIBRARY_URL = import.meta.env.VITE_API_LIBRARY_URL
 
 const LibraryApi = axios.create({
   baseURL: LIBRARY_URL,
-  timeout: 1000,
+  timeout: 4000,
   headers: {
     'Content-Type': 'application/json',
   },
