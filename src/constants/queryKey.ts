13d0ed32d273120c6c28e979404669e5fa40c287
kim dong hyeon <kdh990315@naver.com>
2025-03-02 15:41:01 +0900

    feat: 쿼리 키 파일 추가

---
+++ src/constants/queryKey.ts
@@ -0,0 +1,9 @@
+const BOOKS = {
+  bookList: (searchKeyword: string, searchType: string) => ['bookList', searchKeyword, searchType],
+}
+
+const QUERY_KEY = {
+  BOOKS,
+}
+
+export default QUERY_KEY
