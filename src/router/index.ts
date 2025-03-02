918e9ca83ccae5b469f822bf2fd62155d19e691c
Jiwoo <erase0250@gmail.com>
2025-03-02 15:24:02 +0900

    fix : src/router/index.ts 충돌 해결 & 커뮤니티 상세페이지 테스트용 경로 추가

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,7 @@ import LibraryView from '@/views/LibraryView.vue'
 import PerformanceView from '@/views/PerformanceView.vue'
 import ChallengeView from '@/views/ChallengeView.vue'
 import PerformanceSearchView from '@/views/PerformanceSearchView.vue'
+import CommunityDetailView from '@/views/CommunityDetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -42,6 +43,13 @@ const router = createRouter({
       name: 'community',
       component: CommunityView,
     },
+
+    // UI 확인용 임시 경로 -> 수정 예정
+    {
+      path: '/communityDetail',
+      name: 'communityDetail',
+      component: CommunityDetailView,
+    },
     {
       path: '/login',
       name: 'login',
@@ -51,11 +59,6 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterView,
-    },
-    {
-      path: '/community',
-      name: 'community',
-      component: CommunityView,
     },
     {
       path: '/book',
