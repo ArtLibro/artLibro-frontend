caed10980a4f23558c45b0d349583d9fe3f81033
kim dong hyeon <kdh990315@naver.com>
2025-03-02 14:29:03 +0900

    feat: TanStack Query 설정

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,13 @@ import { createPinia } from 'pinia'
 import Antd from 'ant-design-vue'
 import App from './App.vue'
 import router from './router'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 const app = createApp(App)
 
 app.use(Antd)
 app.use(createPinia())
 app.use(router)
+app.use(VueQueryPlugin)
 
 app.mount('#app')
