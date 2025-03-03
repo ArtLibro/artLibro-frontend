f309f80fe2e51f556e053801d0d2266cd39e89d2
jaeyo03 <toyazx@naver.com>
2025-03-04 02:28:19 +0900

    feat : Kopis API 엔드포인트 추가

--- src/apis/endpoint.ts
+++ src/apis/endpoint.ts
@@ -1,18 +1,29 @@
 const LIBRARY_KEY = import.meta.env.VITE_API_LIBRARY_KEY
+const KOPIS_KEY = import.meta.env.VITE_KOPIS_KEY
 
 /**
 # ======================#
-|       RESERVATION     |
+|       LIBRARY    |
 # ======================#
 */
-
-// export const LIBRARY_ENDPOINT = Object.freeze({
-//   bookList: (keyword: string, pageNo: number, pageSize: number) =>
-//     `/srchBooks?authKey=${LIBRARY_KEY}&keyword=${keyword}&pageNo=${pageNo}&pageSize=${pageSize}&format=json`,
-// })
 
 export const LIBRARY_ENDPOINT = Object.freeze({
   bookList: `/srchBooks?authKey=${LIBRARY_KEY}`,
   libraryDetail : `/extends/libSrch?authKey=${LIBRARY_KEY}`,
   libraryPopularBook : `/extends/loanItemSrchByLib?authKey=${LIBRARY_KEY}`,
+})
+
+/**
+ # ======================#
+ |       KOPIS    |
+ # ======================#
+ */
+
+const today = new Date();
+const firstDate = `${today.getFullYear()}${String(today.getMonth() + 1).padStart(2, '0')}01`;
+const todayDate = `${today.getFullYear()}${String(today.getMonth() + 1).padStart(2, '0')}${String(today.getDate()).padStart(2, '0')}`;
+
+export const KOPIS_ENDPOINT = Object.freeze({
+  awardList : `/prfawad?service=${KOPIS_KEY}&stdate=${firstDate}&eddate=${todayDate}&cpage=1&rows=5`,
+  monthlyList : `/boxoffice?service=${KOPIS_KEY}&stdate=${firstDate}&eddate=${todayDate}&cpage=1&rows=5`,
 })
