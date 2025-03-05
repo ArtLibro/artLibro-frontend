596c5d094c483f364a700529fd6fbbb11f80ff26
Jiwoo <erase0250@gmail.com>
2025-03-06 07:03:56 +0900

    Feat : 기본 타입 외 id, createdAt, image 제외한 타입 추가

--- src/types/community/communityType.ts
+++ src/types/community/communityType.ts
@@ -1,5 +1,6 @@
 // 커뮤니티 타입
 
+// 기존 Post 타입
 export interface Post {
   id: string
   category: '도서' | '공연/행사'
@@ -8,3 +9,6 @@ export interface Post {
   image: string | null
   createdAt: string
 }
+
+// id, createdAt, image 제외한 타입 정의
+export type NewPost = Omit<Post, 'id' | 'createdAt' | 'image'>
