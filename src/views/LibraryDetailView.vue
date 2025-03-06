3f7d8f1e8f733a40a340780d9dd8523f5244a67c
jaeyo03 <toyazx@naver.com>
2025-03-07 01:44:15 +0900

    feat : 도서관 상세 페이지 구현 완료

--- src/views/LibraryDetailView.vue
+++ src/views/LibraryDetailView.vue
@@ -1,38 +1,86 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
-import { onMounted, ref } from 'vue'
-import { getLibraryInfoByLibraryCode, getLibraryPopularBooks } from '@/apis/books.ts'
+import { computed, onMounted, ref } from 'vue'
+import { getBookDetail, getLibraryInfoByLibraryCode, getLibraryPopularBooks } from '@/apis/books.ts'
 import { KakaoMap, KakaoMapMarker } from 'vue3-kakao-maps';
 import type {
   LibraryResult,
   Libraries,
-  LibraryPopularBooksType
+  LibraryPopularBooksType, PopularBookResponse
 } from '@/types/library/library.types.ts'
 import LibraryInfo from '@/components/LibraryDetailView/LibraryInfo.vue'
-import type { Book } from '@/types/libraryType.ts'
+import type { Book, GetBookDetailResponse } from '@/types/libraryType.ts'
 import HomeBookItem from '@/components/common/HomeBookItem.vue'
+
+interface PopularBooksType {
+  age8Books : Book[];
+  age14Books : Book[];
+  age20Books : Book[];
+  loanBooks : Book[];
+}
+
+type AgeBooksType = keyof PopularBooksType;
 
 const route = useRoute();
 const libId = Number(route.params.id);
 const libraryData = ref<Libraries>();
-const popularBooks = ref<Book[]>([]);
+const popularBooksDetail = ref<GetBookDetailResponse[]>([]);
+const popularBooks = ref<PopularBooksType>({
+  age8Books : [],
+  age14Books : [],
+  age20Books : [],
+  loanBooks : [],
+});
+const currentAgeFilter = ref<AgeBooksType>('age20Books');
+const selectedPopularBook = ref<string>('');
+
+function insertDataToArray (books : PopularBookResponse[], array : Book[]) {
+  for (let i = 0; i < books.length; i++) {
+    array.push(books[i].book)
+  }
+  return array;
+}
+
+const handleTabClick = (selected : AgeBooksType) => {
+  currentAgeFilter.value = selected;
+}
+
+const handleBookClick = (isbn13 : string) => {
+  selectedPopularBook.value = isbn13;
+}
+
+const filteredBooks = computed(() =>
+  popularBooksDetail.value.filter(item => selectedPopularBook.value === item.detailData.isbn13)
+);
 
 onMounted(async () => {
   try {
     const data : LibraryResult = await getLibraryInfoByLibraryCode(libId);
     libraryData.value = data.libs[0];
     const books : LibraryPopularBooksType = await getLibraryPopularBooks(libId);
-    const arr = [];
-    for (let i = 0; i < 20; i++) {
-      arr.push(books.loanBooks[i].book)
+    console.log(books);
+    const arrAge8 : Book[] = [];
+    const arrAge14 : Book[] = [];
+    const arrAge20 : Book[] = [];
+    const arrAll : Book[] = [];
+    popularBooks.value.age8Books = insertDataToArray(books.age8Books, arrAge8);
+    popularBooks.value.age14Books = insertDataToArray(books.age14Books,arrAge14);
+    popularBooks.value.age20Books = insertDataToArray(books.age20Books,arrAge20);
+    popularBooks.value.loanBooks = insertDataToArray(books.loanBooks,arrAll);
+
+    const promiseArray : Promise<GetBookDetailResponse>[] = [];
+
+    for (let i = 0; i < 6; i++) {
+      const item = popularBooks.value.loanBooks[i];
+      promiseArray.push(getBookDetail(Number(item.isbn13))) // TODO - get 함수의 인자 타입을 string 으로 바꾸는게 나을듯
     }
-    popularBooks.value = arr;
+
+    popularBooksDetail.value = await Promise.all(promiseArray);
+    selectedPopularBook.value = popularBooksDetail.value[0].detailData.isbn13;
   } catch (error) {
     console.error('API 요청 실패:', error);
   }
 });
-
-
 </script>
 
 <template>
@@ -49,10 +97,45 @@ onMounted(async () => {
         :tel="libraryData.lib.libInfo.tel"
       />
       <div class="popular-books">
-        <h2>도서관 인기 도서 TOP20</h2>
-        <div class="books-grid">
+        <h2>도서관 인기 도서</h2>
+        <div class="flex-wrapper">
+          <div class="books-grid">
+            <div v-for="item in popularBooksDetail" :key="item.detailData.isbn13">
+              <img
+                :src="item.detailData.bookImageURL"
+                height="237"
+                width="206"
+                alt="book-img"
+                @click="handleBookClick(item.detailData.isbn13)"
+                class="book-image"
+                :class="{'book-image-shadow' : selectedPopularBook !== item.detailData.isbn13}"
+              >
+            </div>
+          </div>
+          <div
+            v-for="item in filteredBooks"
+            :key="item.detailData.isbn13"
+            class="popular-book-info"
+          >
+            <div class="book-info-title">
+              <img src="/icons/library/purple-box.svg" alt="block-img">
+              <div>{{item.detailData.bookname}}</div>
+            </div>
+            <div class="author">{{item.detailData.authors}}</div>
+            <div class="description">{{item.detailData.description}}</div>
+          </div>
+        </div>
+      </div>
+      <div class="popular-books">
+        <div class="title-with-tab">
+          <h2>도서관 연령별 인기 도서</h2>
+          <div :class="{tab : true, 'tab-active': currentAgeFilter === 'age20Books'}" @click="handleTabClick('age20Books')">성인</div>
+          <div :class="{tab : true, 'tab-active': currentAgeFilter === 'age14Books'}" @click="handleTabClick('age14Books')">청소년</div>
+          <div :class="{tab : true, 'tab-active': currentAgeFilter === 'age8Books'}" @click="handleTabClick('age8Books')">영유아</div>
+        </div>
+        <div class="books-flex">
           <HomeBookItem
-            v-for="book in popularBooks"
+            v-for="book in popularBooks[currentAgeFilter]"
             :authors="book.authors"
             :title="book.bookname"
             :bookimage="book.bookImageURL"
@@ -66,7 +149,9 @@ onMounted(async () => {
           :lat="Number(libraryData.lib.libInfo.latitude)"
           :lng="Number(libraryData.lib.libInfo.longitude)"
           title="절대경로로 이미지 가져오기"
-        />
+        >
+          <template v-slot:infoWindow><div style="padding: 10px">{{ libraryData.lib.libInfo.libName }}</div></template>
+        </KakaoMapMarker>
       </KakaoMap>
     </div>
     <div v-else>
@@ -81,20 +166,21 @@ onMounted(async () => {
   width: 1246px;
 }
 
-.books-grid {
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  padding: 50px 30px 20px;
+.books-flex {
+  display: flex;
+  flex-wrap: nowrap; /* 한 줄로 유지 */
+  gap: 20px; /* 요소들 간의 간격 (원하는 값으로 조정) */
+  padding: 40px;
   width: 1246px;
-  height: 1360px;
+  height: 370px;
   background-color: #FCF4EF;
   border-radius: 20px;
-  margin-top : 10px;
-  justify-items: center;
+  margin-top: 10px;
+  overflow-x: auto; /* 가로 스크롤 활성화 */
 }
 
 .popular-books {
-  margin-top: 40px;
+  margin-top: 80px;
 }
 
 .info-title {
@@ -109,5 +195,102 @@ onMounted(async () => {
   margin-top : 10px;
   margin-bottom : 40px;
   border-radius: 10px;
+}
+
+.title-with-tab {
+  display: flex;
+  gap: 40px;
+  align-items: center;
+  height: 36px;
+  width: 470px;
+}
+
+.tab {
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  width: 42px;
+  height: 19px;
+  cursor: pointer;
+  font-size: 16px;
+  color: $text-color-400;
+  font-weight: 500;
+  position: relative; /* 자식 요소(.tab-active::before)의 위치 기준 */
+}
+
+.tab:hover {
+  color: $secondary-color-300;
+}
+
+.tab-active {
+  color: $secondary-color-300;
+}
+
+.tab-active::before {
+  content: "";
+  position: absolute;
+  top: -10px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 8px;
+  height: 8px;
+  background-color: $primary-color-100;
+  border-radius: 50%;
+}
+
+.flex-wrapper {
+  display: flex;
+  height: 497px;
+  width : 1246px;
+  justify-content: space-between;
+  margin-top: 10px;
+}
+
+.books-grid {
+  display: grid;
+  gap : 20px;
+  grid-template-columns: repeat(3, 1fr);
+  height: 497px;
+  width: 760px;
+}
+
+.popular-book-info {
+  display: grid;
+  height: 497px;
+  width: 450px;
+  place-content: center;
+  gap: 20px;
+}
+
+.book-image {
+  cursor: pointer;
+}
+
+.book-image-shadow {
+  filter: brightness(45%)
+}
+
+.author {
+  display: flex;
+  width: 450px;
+  font-size: 20px;
+  color:  $text-color-500;
+  font-weight: 600;
+  justify-content: flex-end;
+}
+
+.description {
+  font-size: 18px;
+  color: $text-color-500;
+}
+
+.book-info-title {
+  display: flex;
+  gap: 40px;
+  font-size: 36px;
+  font-weight: bold;
+  align-items: center;
+  width: 450px;
+  height: 43px;
 }
 </style>
